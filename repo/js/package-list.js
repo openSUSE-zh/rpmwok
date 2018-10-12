d1fe5c105c241e4d85cd8770b41f6437363fe9b2
Guo Yunhe <yunhe.guo@protonmail.com>
2018-10-13 01:40:03 +0300

    Add package download links

--- repo/js/package-list.js
+++ repo/js/package-list.js
@@ -1,7 +1,11 @@
 var packageList = document.getElementById('package-list');
 packages.map(function (pkg) {
     var packageItem = document.createElement('li');
+    var packageLink = document.createElement('a');
     var packageName = document.createTextNode(pkg);
-    packageItem.appendChild(packageName);
+    packageLink.appendChild(packageName);
+    packageLink.href = pkg;
+    packageLink.download = pkg;
+    packageItem.appendChild(packageLink);
     packageList.appendChild(packageItem);
 })
